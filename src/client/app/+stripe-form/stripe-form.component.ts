0739e9df892109324d7350bc8ce6afbf3aa9a968
mgechev <mgechev@gmail.com>
2016-07-05 19:44:16 +0300

    Fix stripe default checkout

--- src/client/app/+stripe-form/stripe-form.component.ts
+++ src/client/app/+stripe-form/stripe-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer } from '@angular/core';
 import { SafeHtml, DomSanitizationService } from '@angular/platform-browser';
 
 const STRIPE_SCRIPT =
@@ -26,8 +26,35 @@ const STRIPE_SCRIPT =
 })
 export class StripeFormComponent {
   stripeScript: SafeHtml;
-  constructor(private sanitizer: DomSanitizationService) {
+  globalListener: any;
+
+  constructor(private sanitizer: DomSanitizationService, private renderer: Renderer) {
     this.stripeScript = sanitizer.bypassSecurityTrustHtml(STRIPE_SCRIPT);
+  }
+
+  openCheckout() {
+    var handler = (<any>window).StripeCheckout.configure({
+      key: 'pk_test_oi0sKPJYLGjdvOXOM8tE8cMa',
+      locale: 'auto',
+      token: function (token: any) {
+        // You can access the token ID with `token.id`.
+        // Get the token ID to your server-side code for use.
+      }
+    });
+
+    handler.open({
+      name: 'Demo Site',
+      description: '2 widgets',
+      amount: 2000
+    });
+
+    this.globalListener = this.renderer.listenGlobal('window', 'popstate', () => {
+      handler.close();
+    });
+  }
+
+  ngOnDestroy() {
+    this.globalListener();
   }
 }
 
