9b254d23ab70b7512d71ee3bfa45281dcf4546e7
mgechev <mgechev@gmail.com>
2016-07-05 20:07:49 +0300

    Remove unused imports

--- src/client/app/+stripe-form/stripe-form.component.ts
+++ src/client/app/+stripe-form/stripe-form.component.ts
@@ -1,23 +1,5 @@
 import { Component, Renderer } from '@angular/core';
-import { SafeHtml, DomSanitizationService } from '@angular/platform-browser';
 
-const STRIPE_SCRIPT =
-`
-<form action="" method="POST">
-  <script
-    src="https://checkout.stripe.com/checkout.js" class="stripe-button"
-    data-key="pk_test_oi0sKPJYLGjdvOXOM8tE8cMa"
-    data-amount="999"
-    data-name="Demo Site"
-    data-description="Widget"
-    data-locale="auto">
-  </script>
-</form>
-`;
-
-/**
- * This class represents the lazy loaded AboutComponent.
- */
 @Component({
   moduleId: module.id,
   selector: 'sd-stripe-form',
@@ -25,12 +7,9 @@ const STRIPE_SCRIPT =
   styleUrls: ['stripe-form.component.css']
 })
 export class StripeFormComponent {
-  stripeScript: SafeHtml;
   globalListener: any;
 
-  constructor(private sanitizer: DomSanitizationService, private renderer: Renderer) {
-    this.stripeScript = sanitizer.bypassSecurityTrustHtml(STRIPE_SCRIPT);
-  }
+  constructor(private renderer: Renderer) {}
 
   openCheckout() {
     var handler = (<any>window).StripeCheckout.configure({
