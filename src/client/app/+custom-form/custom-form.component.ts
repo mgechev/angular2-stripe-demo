2ebcd832448cff87042ad72b2cc2d70336aeed3d
mgechev <mgechev@gmail.com>
2016-07-05 10:05:50 +0300

    Add custom form

--- src/client/app/+custom-form/custom-form.component.ts
+++ src/client/app/+custom-form/custom-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 import { REACTIVE_FORM_DIRECTIVES } from '@angular/forms';
 
 /**
@@ -12,5 +12,32 @@ import { REACTIVE_FORM_DIRECTIVES } from '@angular/forms';
   directives: [REACTIVE_FORM_DIRECTIVES]
 })
 export class CustomFormComponent {
+  cardNumber: string;
+  expiryMonth: string;
+  expiryYear: string;
+  cvc: string;
 
+  message: string;
+
+  constructor(private _zone: NgZone) {}
+
+  getToken() {
+    this.message = 'Loading...';
+
+    (<any>window).Stripe.card.createToken({
+      number: this.cardNumber,
+      exp_month: this.expiryMonth,
+      exp_year: this.expiryYear,
+      cvc: this.cvc
+    }, (status: number, response: any) => {
+      this._zone.run(() => {
+        if (status === 200) {
+          this.message = `Success! Card token ${response.card.id}.`;
+        } else {
+          this.message = response.error.message;
+        }
+      });
+    });
+  }
 }
+
